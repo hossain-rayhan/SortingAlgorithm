ff64954b62c568b3cc36f332bff4f3c479a9f849
hossain-rayhan <hossain.rayhan@outlook.com>
2018-12-28 17:32:41 -0500

    Selection Sort implemented in JAVA

--- SelectionSort.java
+++ SelectionSort.java
@@ -9,21 +9,24 @@ import java.util.Arrays;
 
 class SelectionSort{
 	public static void main(String[] args){
-		int[] arr = {11, -5, 2, 15, 3, 1, 6};
+		int[] arr = {11, -5, 2, 15, 3, 1, 6, - 2};
 
 		doSelectionSort(arr);
 		System.out.println(Arrays.toString(arr));
 	}
 	
 	public static void doSelectionSort(int arr[]){
-		for(int i = 0; i < arr.length; i++){
-			for(int j = 0; j < arr.length - i - 1; j++){
-				if(arr[j] > arr[j+1]){
-					int temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
+		for(int i = 0; i < arr.length-1; i++){
+			int minIndex = i;
+			for(int j = i+1; j < arr.length; j++){
+				if(arr[j] < arr[minIndex]){
+					minIndex = j;
 				}
 			}
+
+			int temp = arr[i];
+			arr[i] = arr[minIndex];
+			arr[minIndex] = temp;
 		}
 	}
 }
